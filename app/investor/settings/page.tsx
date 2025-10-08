6c2dfb080f7562512b6590429a3eced3d99ad66a
abz1717 <2457570@dundee.ac.uk>
2025-10-08 14:24:00 +0100

    fixing vercel error

--- app/investor/settings/page.tsx
+++ app/investor/settings/page.tsx
@@ -1,10 +1,12 @@
+import { Suspense } from "react";
 import { AccountSettings } from "@/components/settings/account-settings";
 
 export default function BusinessSettingsPage() {
-
   return (
-    <div className="container mx-auto py-6">
-      <AccountSettings />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="container mx-auto py-6">
+        <AccountSettings />
+      </div>
+    </Suspense>
   );
 }
